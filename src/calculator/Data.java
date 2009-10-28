9612f2c35fca0707c581907cd8367c89433f6da6
mpreda <mpreda@localhost>
2009-10-29 00:33:23 +0100

    graph: binary split, start add bitmap.

---
+++ src/calculator/Data.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2009 Mihai Preda
+
+package calculator;
+
+class Data {
+    float[] xs = new float[4];
+    float[] ys = new float[4];
+    int size = 0;
+    int allocSize = 4;
+
+    void push(float x, float y) {
+        if (size >= allocSize) {
+            allocSize += allocSize;
+            Calculator.log("resize " + size + ' ' + allocSize);
+            float[] a = new float[allocSize];
+            System.arraycopy(xs, 0, a, 0, size);
+            xs = a;
+            a = new float[allocSize];
+            System.arraycopy(ys, 0, a, 0, size);
+            ys = a;
+        }
+        xs[size] = x;
+        ys[size] = y;
+        ++size;
+    }
+
+    float topX() {
+        return xs[size-1];
+    }
+
+    float topY() {
+        return ys[size-1];
+    }
+
+    void pop() {
+        --size;
+    }
+
+    boolean empty() {
+        return size == 0;
+    }
+
+    void clear() {
+        size = 0;
+    }
+}
--- src/calculator/GraphView.java
+++ src/calculator/GraphView.java
@@ -35,97 +35,112 @@ public class GraphView extends View {
     private int width, height;
     private Matrix matrix = new Matrix();
     private Paint paint = new Paint();
-    // private Symbols symbols;
-    // private float minX = -3, maxX = 3;
-    static private int NPOINTS = 30;
-    //private float ys[] = new float[NPOINTS];
-    private Path path = new Path(), transPath = new Path();
-    Function function;
+    private Function function;
+    private Data next = new Data(), graph = new Data();
+    private boolean invalidated = true;
+    private Bitmap bitmap;
 
     public GraphView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        paint.setAntiAlias(false);
+        paint.setAntiAlias(false);        
     }
 
     void setFunction(Function f) {
         this.function = f;
+        invalidated = true;
     }
 
     protected void onSizeChanged(int w, int h, int ow, int oh) {
         width = w;
         height = h;
+        invalidated = true;
+        bitmap = null;
     }
 
-    private boolean isGood(float v, float minY, float maxY) {
-        // return v != Float.NEGATIVE_INFINITY && v != Float.POSITIVE_INFINITY && v != Float.NaN;
-        return minY <= v && v <= maxY;
-        // Float.NEGATIVE_INFINITY < v && v < Float.POSITIVE_INFINITY &&
-    }
-
-    private float goodness(float a, float b, float c, float minY, float maxY) {
-        return (isGood(a, minY, maxY) && isGood(b, minY, maxY) && isGood(c, minY, maxY)) ? 
-            Math.abs(a + c - b - b) : 0;
-    }
-
-    private static final int N = 12;
-    private float goodness(Function f, float minX, float maxX, float minY, float maxY) {
-        float step = (maxX - minX) / (N - 1);
-        float a, b = (float) f.eval(minX), c = (float) f.eval(minX + step);
-        float x = minX + step + step;
-        float sum = 0;
-        for (int i = 0; i < N - 2; ++i, x += step) {
-            a = b;
-            b = c;
-            c = (float) f.eval(x);
-            sum += goodness(a, b, c, minY, maxY);
-        }
-        return sum / (maxY - minY);
-    }
-
-    private static final float VARS[] = {1, 3, 10};
     public void draw(Canvas canvas) {
         super.draw(canvas);
         if (function == null) {
             return;
         }
+        if (invalidated) {
+            drawBitmap();
+        }
+        canvas.drawBitmap(bitmap, 0, 0, null);
+    }
 
-        int nvars = VARS.length;
-        float bestGood = -1;
-        float bestX = -1;
-        for (int i = 0; i < nvars; ++i) {
-            float maxX = VARS[i];
-            float maxY = maxX * height / width;
-            float good = goodness(function, -maxX, maxX, -maxY, maxY);
-            Calculator.log("good " + maxX + ' ' + maxY + ' ' + good);
-            if (good >= bestGood) {
-                bestGood = good;
-                bestX = maxX;
-            }            
-        }                
-        float maxX = bestX;
-        float minX = -maxX;
-        float maxY = maxX * height / width;
-        float minY = -maxY;
-
-        canvas.drawColor(0xffffffff);
-                
-        //float minY = Float.POSITIVE_INFINITY;
-        //float maxY = Float.NEGATIVE_INFINITY;
-
-        paint.setColor(0xffa0ffa0);
-        paint.setStrokeWidth(0);
-        paint.setAntiAlias(false);
-        paint.setStyle(Paint.Style.STROKE);
-
-        path.rewind();
+    private Data computeGraph(float minX, float maxX, float minY, float maxY) {
         final float scale = width / (maxX - minX);
-        final float step = 5 / scale;
+        final float maxStep = 16 / scale;
+        final float minStep = .1f / scale;
+        Calculator.log("step min " + minStep + " max " + maxStep);
+        final float ythresh = 1/scale;
+        next.clear();
+        graph.clear();
+        float leftX = minX, leftY = (float) function.eval(minX);
+        graph.push(leftX, leftY);
+        float rightX, rightY;
+        while (true) {
+            if (next.empty()) {
+                rightX = leftX + maxStep;
+                rightY = (float) function.eval(rightX);
+            } else {
+                rightX = next.topX();
+                rightY = next.topY();
+                next.pop();
+            }
+            if (leftX > maxX) {
+                break;
+            }
+            if (leftY != leftY && rightY != rightY) { // NaN
+                leftX = rightX;
+                leftY = rightY;
+                continue;
+            }
+            float span = rightX - leftX;
+            if (span <= minStep ||
+                (leftY < minY && rightY < minY) ||
+                (leftY > maxY && rightY > maxY)) {
+                graph.push(rightX, rightY);
+                leftX = rightX;
+                leftY = rightY;
+                continue;
+            }
+            if ((leftY < -100 && rightY > 100) || 
+                (leftY > 100 && rightY < -100)) {
+                graph.push(rightX, Float.NaN);
+                graph.push(rightX, rightY);
+                leftX = rightX;
+                leftY = rightY;
+                continue;
+            }
+
+            float middleX = (leftX + rightX) / 2;
+            float middleY = (float) function.eval(middleX);
+            float diff = Math.abs(leftY + rightY - middleY - middleY);
+            if (diff < ythresh || 
+                (leftY < minY && middleY < minY && rightY < minY) ||
+                (leftY > maxY && middleY > maxY && rightY > maxY)) {
+                graph.push(rightX, rightY);
+                leftX = rightX;
+                leftY = rightY;
+            } else {
+                next.push(middleX, middleY);
+            }
+        }
+        return graph;
+    }
+
+    private Path path = new Path();
+    private Path graphToPath(Data graph) {
         boolean first = true;
-        float a = Float.NaN, b = Float.NaN;
-        for (float x = minX; ; x += step) {
-            float y = (float) function.eval(x);
-            //minY = y < minY ? y : minY;
-            //maxY = y > maxY ? y : maxY;
+        int size = graph.size;
+        float[] xs = graph.xs;
+        float[] ys = graph.ys;
+        path.rewind();
+        for (int i = 0; i < size; ++i) {
+            float y = ys[i];
+            float x = xs[i];
+            // Calculator.log("" + x + ' ' + y);
             if (y == y) { // !NaN
                 if (first) {
                     path.moveTo(x, y);
@@ -136,23 +151,34 @@ public class GraphView extends View {
             } else {
                 first = true;
             }
-            if (x > maxX) {
-                break;
-            }
         }
+        return path;
+    }
 
-        // float scaley = scalex; // height / (maxY - minY);
-        /*
-        if (scaley > scalex && scaley/scalex < 1.5) {
-            scaley = scalex;
+    private void drawBitmap() {
+        invalidated = false;
+
+        float maxX = findBestSizeX(function, width, height);
+        float minX = -maxX;
+        float maxY = maxX * height / width;
+        float minY = -maxY;
+        Data graph = computeGraph(minX, maxX, minY, maxY);
+        Path path = graphToPath(graph);        
+
+        if (bitmap == null) {
+            bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
         }
-        */
-        // Calculator.log("scale " + scalex + ' ' + scaley);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(0xffffffff);
+                
+        paint.setColor(0xffa0ffa0);
+        paint.setStrokeWidth(0);
+        paint.setAntiAlias(false);
+        paint.setStyle(Paint.Style.STROKE);
 
-
+        final float scale = width / (maxX - minX);
         matrix.reset();
         matrix.preScale(scale, -scale);
-        // Calculator.log("size " + width + ' ' + height);
         matrix.postTranslate(width/2, height/2);
         canvas.concat(matrix);
 
@@ -170,21 +196,47 @@ public class GraphView extends View {
         paint.setColor(0xff000000);
         paint.setStrokeWidth(0);
         paint.setAntiAlias(true);
-        canvas.drawPath(path, paint);
+        canvas.drawPath(path, paint);        
+    }
 
-        /*
-        paint.setColor(0xffff0000);
-        canvas.drawPoint(10, 10, paint);
-        canvas.drawPath(path, paint);
-        canvas.drawLine(0, 0, 10, 5, paint);
-        
-        // Paint pt = new Paint();
-        paint.setColor(0xff00ff00);
-        Path p = new Path();
-        p.moveTo(20, 20);
-        p.lineTo(100, 50);
-        p.lineTo(110, 100);
-        canvas.drawPath(p, paint);
-        */
+    private static boolean isGood(float v, float minY, float maxY) {
+        return minY <= v && v <= maxY;
+    }
+
+    private static float goodness(float a, float b, float c, float minY, float maxY) {
+        return (isGood(a, minY, maxY) && isGood(b, minY, maxY) && isGood(c, minY, maxY)) ? 
+            Math.abs(a + c - b - b) : 0;
+    }
+
+    private static final int N = 20;
+    private static float goodness(Function f, float minX, float maxX, float minY, float maxY) {
+        float step = (maxX - minX) / (N - 1);
+        float a, b = (float) f.eval(minX), c = (float) f.eval(minX + step);
+        float x = minX + step + step;
+        float sum = 0;
+        for (int i = 0; i < N - 2; ++i, x += step) {
+            a = b;
+            b = c;
+            c = (float) f.eval(x);
+            sum += goodness(a, b, c, minY, maxY);
+        }
+        return sum / (maxY - minY);
+    }
+
+    private static final float VARS[] = {1, 2, 3, 5, 10};
+    private static float findBestSizeX(Function f, int width, int height) {
+        float bestGood = -1;
+        float bestX = -1;
+        final float ratio = height / (float) width;
+        for (float maxX : VARS) {
+            float maxY = maxX * ratio;
+            float good = goodness(f, -maxX, maxX, -maxY, maxY);
+            // Calculator.log("good " + maxX + ' ' + maxY + ' ' + good);
+            if (good >= bestGood) {
+                bestGood = good;
+                bestX = maxX;
+            }
+        }
+        return bestX;
     }
 }
