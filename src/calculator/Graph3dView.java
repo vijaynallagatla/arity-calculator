ded3249d0c6bbb1639f95d7f1f8b27fe56f5aece
mpreda <mpreda@localhost>
2009-11-26 15:43:01 +0100

    fix 3d rotation.

--- src/calculator/Graph3dView.java
+++ src/calculator/Graph3dView.java
@@ -3,11 +3,15 @@ package calculator;
 import android.opengl.GLSurfaceView;
 import android.content.Context;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.widget.Scroller;
 import arity.calculator.R;
 
 public class Graph3dView extends GLSurfaceView {
     private GraphRenderer renderer;
     private float lastTouchX, lastTouchY;
+    private VelocityTracker velocityTracker;
+    private Scroller scroller;
 
     Graph3dView(Context context, Graph3d graph) {
         super(context);
@@ -15,6 +19,7 @@ public class Graph3dView extends GLSurfaceView {
         setRenderer(renderer);
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
         setDebugFlags(1);
+        scroller = new Scroller(context);
     }
 
     public boolean onTouchEvent(MotionEvent event) {
@@ -23,23 +28,41 @@ public class Graph3dView extends GLSurfaceView {
         float y = event.getY();
         switch (action) {
         case MotionEvent.ACTION_DOWN:
+            if (!scroller.isFinished()) {
+                scroller.abortAnimation();
+            }
+            velocityTracker = VelocityTracker.obtain();
+            velocityTracker.addMovement(event);
             lastTouchX = x;
             lastTouchY = y;
             break;
 
         case MotionEvent.ACTION_MOVE:
+            velocityTracker.addMovement(event);
             float deltaX = x - lastTouchX;
             float deltaY = y - lastTouchY;
-            lastTouchX = x;
-            lastTouchY = y;
-            renderer.incAngle(deltaX, deltaY);
-            requestRender();
+            if (deltaX > 1 || deltaX < -1 || deltaY > 1 || deltaY < -1) {
+                renderer.incAngle(deltaX, deltaY);
+                requestRender();
+                lastTouchX = x;
+                lastTouchY = y;
+            }
             break;
             
         case MotionEvent.ACTION_UP:
+            velocityTracker.computeCurrentVelocity(1000);
+            scroller.fling(Math.round(x), Math.round(y), 
+                           Math.round(velocityTracker.getXVelocity()), 
+                           Math.round(velocityTracker.getYVelocity()),
+                           -1000, 1000, -1000, 1000);
+            requestRender();
             // no break
 
         default:
+            if (velocityTracker != null) {
+                velocityTracker.recycle();
+                velocityTracker = null;
+            }
         }
         return true;
     }
--- src/calculator/GraphRenderer.java
+++ src/calculator/GraphRenderer.java
@@ -8,46 +8,67 @@ import javax.microedition.khronos.opengles.GL10;
 import javax.microedition.khronos.opengles.GL11;
 
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 class GraphRenderer implements GLSurfaceView.Renderer {
     Graph3d graph;
     private float angleX, angleY = 15;
     private long lastTime;
     private int drawCnt = 1;
+    private float[] matrix1 = new float[16], matrix2 = new float[16], matrix3 = new float[16];
 
     public GraphRenderer(Graph3d graph) {
         this.graph = graph;
+        Matrix.setIdentityM(matrix1, 0);
+        Matrix.rotateM(matrix1, 0, -75, 1, 0, 0);
     }
 
     public void incAngle(float dx, float dy) {
-        angleX += dx;
-        angleY += dy;
-        angleY = Math.max(angleY, -90);
-        angleY = Math.min(angleY, 90);
+        angleX = dx;
+        angleY = dy;
     }
 
-    public void onDrawFrame(GL10 gl) {
-        // gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);        
+    public void onDrawFrame(GL10 gl10) {
+        GL11 gl = (GL11) gl10;
+        /*
         if (--drawCnt == 0) {
             drawCnt = 30;
             long now = System.currentTimeMillis();
             Calculator.log("time " + ((now - lastTime) / 3));
             lastTime = now;
         }
+        */
 
+        // gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);        
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         gl.glMatrixMode(GL10.GL_MODELVIEW);
         gl.glLoadIdentity();
         gl.glTranslatef(0, 0, -(NEAR+6.5f));
-        gl.glRotatef(-90+angleY, 1, 0, 0);        
-        gl.glRotatef(angleX, 0, 0, 1);        
+
+        Matrix.setIdentityM(matrix2, 0);
+        if (angleY < .1f) {
+            Matrix.rotateM(matrix2, 0, angleX, 0, 1, 0);
+            Matrix.rotateM(matrix2, 0, angleY, 1, 0, 0);
+        } else if (angleX < .1f) {
+            Matrix.rotateM(matrix2, 0, angleY, 1, 0, 0);
+            Matrix.rotateM(matrix2, 0, angleX, 0, 1, 0);
+        } else {
+            float halfX = angleX/2;
+            Matrix.rotateM(matrix2, 0, halfX, 0, 1, 0);
+            Matrix.rotateM(matrix2, 0, angleY, 1, 0, 0);
+            Matrix.rotateM(matrix2, 0, halfX, 0, 1, 0);
+        }
+        Matrix.multiplyMM(matrix3, 0, matrix2, 0, matrix1, 0);
+        gl.glMultMatrixf(matrix3, 0);
+        
+        System.arraycopy(matrix3, 0, matrix1, 0, 16);
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);        
         graph.draw(gl);
-        // angle += 1f;
     }
 
     private static final int NEAR = 15;
     public void onSurfaceChanged(GL10 gl, int width, int height) {
+        Calculator.log("onSurfaceChanged");
         gl.glViewport(0, 0, width, height);
         gl.glMatrixMode(GL10.GL_PROJECTION);
         gl.glLoadIdentity();
@@ -59,6 +80,7 @@ class GraphRenderer implements GLSurfaceView.Renderer {
 
 
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        Calculator.log("onSurfaceCreated");
         gl.glDisable(GL10.GL_DITHER);
         gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);               
         gl.glClearColor(0, 0, 0, 1);
