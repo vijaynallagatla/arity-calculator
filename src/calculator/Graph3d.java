4d2527cbd29191ea32fb7f93098dbeaffd432d05
mpreda <mpreda@localhost>
2009-11-30 16:58:17 +0100

    optimize wireframe drawing.

--- src/calculator/Graph3d.java
+++ src/calculator/Graph3d.java
@@ -13,36 +13,29 @@ import javax.microedition.khronos.opengles.GL10;
 import org.javia.arity.*;
 
 class Graph3d {
-    private static final int N = 25;
+    private static final int N = 26;
     private float minX = -4, maxX = 4, minY = -4, maxY = 4; 
 
     private FloatBuffer vertexBuf, colorBuf, axisVertexBuf, gridVertexBuf;
-    private ShortBuffer verticeIdx[] = new ShortBuffer[N];
+    private ShortBuffer verticeIdx;;
     
     public Graph3d() {
-        short[] b = new short[N];
-        for (int i = 0; i < N; ++i) {
-            short v = (short) i;
-            for (int j = 0; j < N; ++j, v+=N) {
-                b[j] = v;
+        short[] b = new short[N*N];
+        int p = 0;
+        for (int i = 0; i < N; i++) {
+            short v = 0;
+            for (int j = 0; j < N; v += N+N, j+=2) {
+                b[p++] = (short)(v+i);
+                b[p++] = (short)(v+N+N-1-i);
             }
-            verticeIdx[i] = buildBuffer(b);
+            v = N*(N-2);
+            i++;
+            for (int j = N-1; j >= 0; v -= N+N, j-=2) {
+                b[p++] = (short)(v+N+N-1-i);
+                b[p++] = (short)(v+i);
+            } 
         }
-
-        float grid[] = {
-            -4, -4, 0, -4, 4, 0,
-            -2, -4, 0, -2, 4, 0,
-            0, -4, 0, 0, 4, 0,
-            2, -4, 0, 2, 4, 0,
-            4, -4, 0, 4, 4, 0,
-
-            -4, -4, 0, 4, -4, 0,
-            -4, -2, 0, 4, -2, 0,
-            -4, 0, 0, 4, 0, 0,
-            -4, 2, 0, 4, 2, 0,
-            -4, 4, 0, 4, 4, 0,
-        };
-        gridVertexBuf = buildBuffer(grid);
+        verticeIdx = buildBuffer(b);
 
         float unit = 5;
         float axis[] = {
@@ -53,7 +46,24 @@ class Graph3d {
             0, 0, 0,
             0, 0, unit,
         };
-        axisVertexBuf = buildBuffer(axis);        
+        axisVertexBuf = buildBuffer(axis);
+        fillGrid();
+    }
+
+    private void fillGrid() {
+        float[] grid = new float[18 * 6];
+        int p = 0;
+        for (int i = -4; i <= 4; ++i) {
+            grid[p] = i; grid[p+1] = -4; grid[p+2] = 0;
+            p += 3;
+            grid[p] = i; grid[p+1] = 4; grid[p+2] = 0;
+            p += 3;
+            grid[p] = -4; grid[p+1] = i; grid[p+2] = 0;
+            p += 3;
+            grid[p] = 4; grid[p+1] = i; grid[p+2] = 0;
+            p += 3;
+        }
+        gridVertexBuf = buildBuffer(grid);
     }
 
     public void setFunction(Function f) {
@@ -67,9 +77,11 @@ class Graph3d {
             int pos = 0;
             float maxAbs = 0;
             float y = minY;
-            for (int i = 0; i < N; ++i, y+=stepY) {
-                float x = minX;
-                for (int j = 0; j < N; ++j, x+=stepX, pos+=3) {
+            float x = minX - stepX;
+            for (int i = 0; i < N; i++, y+=stepY) {
+                float xinc = (i & 1) == 0 ? stepX : -stepX;
+                x += xinc;
+                for (int j = 0; j < N; ++j, x+=xinc, pos+=3) {
                     float z = (float) f.eval(x, y);
                     if (z != z) {
                         z = 0;
@@ -82,6 +94,8 @@ class Graph3d {
                     }
                 }
             }
+
+
             for (int i = (N*N*3-3); i >= 0; i-=3) {
                 float z = vertices[i+2];
                 float a = z / maxAbs;
@@ -117,7 +131,7 @@ class Graph3d {
         gl.glColor4f(.7f, .7f, .7f, 1);
 
         gl.glVertexPointer(3, gl.GL_FLOAT, 0, gridVertexBuf);        
-        gl.glDrawArrays(gl.GL_LINES, 0, 20);
+        gl.glDrawArrays(gl.GL_LINES, 0, gridVertexBuf.capacity()/3);
 
         gl.glColor4f(1, 1, 1, 1);
         gl.glVertexPointer(3, gl.GL_FLOAT, 0, axisVertexBuf);
@@ -128,14 +142,7 @@ class Graph3d {
         gl.glVertexPointer(3, gl.GL_FLOAT, 0, vertexBuf);
         gl.glColorPointer(3, gl.GL_FLOAT, 0, colorBuf);
 
-        // gl.glDrawElements(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_SHORT, cube);
-        // gl.glColor4f(1, 1, 1, 1);
-        for (int i = 0; i < N; ++i) {
-            gl.glDrawArrays(gl.GL_LINE_STRIP, i*N, N);
-            gl.glDrawElements(gl.GL_LINE_STRIP, N, GL10.GL_UNSIGNED_SHORT, verticeIdx[i]);
-        }
-
-        // gl.glColor4f(0, 1, 0, 1);
-        // gl.glDrawElements(gl.GL_LINES, 6, gl.GL_UNSIGNED_SHORT, axis);
+        gl.glDrawArrays(gl.GL_LINE_STRIP, 0, N*N);
+        gl.glDrawElements(gl.GL_LINE_STRIP, N*N, GL10.GL_UNSIGNED_SHORT, verticeIdx);
     }
 }
