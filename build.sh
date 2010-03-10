cb7d1feaf0dc344f0726798f355842c032e68ec3
mpreda <mpreda@localhost>
2010-03-10 21:31:02 +0100

    v1.22; support Cupcake & Donut.

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ echo apkbuilder
 apkbuilder $OUT -u -z $PKRES -f bin/classes.dex
 
 echo jarsigner
-jarsigner -keystore $KEYSTORE -storepass 000000 $OUT $KEYALIAS || jarsigner -keystore $KEYSTORE $OUT $KEYALIAS
+jarsigner -keystore $KEYSTORE -storepass 000000 $OUT $KEYALIAS > /dev/null || jarsigner -keystore $KEYSTORE $OUT $KEYALIAS
 
 echo zipalign
 zipalign -f 4 $OUT $ALIGNOUT
